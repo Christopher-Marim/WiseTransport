acc829dc542b3bd5c50c46b3584590d97f9d1fe0
Christopher-Marim <cssmarim1@gmail.com>
2021-08-05 15:51:37 -0300

    Update commonsVariables.js

--- commonsVariables.js
+++ commonsVariables.js
@@ -1,6 +1,6 @@
 export default {
         api:{
-            baseUrl:'http://transportadora.etm.ltda/',
-            Authorization: 'Basic ac0fb7c1dedf6eb4cb16e4dab5fac37a63bf447f74a8c47366f9e7f5d72d'
+            baseUrl:'zzzzzz',
+            Authorization: 'zzzzzzz'
         }
 }
